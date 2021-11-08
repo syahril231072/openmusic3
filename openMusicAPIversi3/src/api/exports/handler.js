ca3b31fc51da296c8266f33048be46499d91c07b
syahril231072 <syahril231072@gmail.com>
2021-11-08 14:11:01 +0700

    final

---
+++ openMusicAPIversi3/src/api/exports/handler.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+const ClientError = require('../../exceptions/ClientError');
+
+class ExportsHandler {
+  constructor(service, validator, playlistsService) {
+    this._service = service;
+    this._validator = validator;
+    this._playlistsService = playlistsService;
+
+    this.postExportNotesHandler = this.postExportNotesHandler.bind(this);
+  }
+
+  async postExportNotesHandler(request, h) {
+    try {
+      this._validator.validateExportNotesPayload(request.payload);
+      const { playlistId } = request.params;
+      const { id: userId } = request.auth.credentials;
+
+      await this._playlistsService.verifyPlaylistAccess(playlistId, userId);
+
+      const message = {
+        playlistId,
+        targetEmail: request.payload.targetEmail,
+      };
+
+      await this._service.sendMessage('export:songs', JSON.stringify(message));
+
+      const response = h.response({
+        status: 'success',
+        message: 'Permintaan Anda dalam antrean',
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = ExportsHandler;
---
+++ openMusicAPIversi3/src/services/postgres/CollaborationsService.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-underscore-dangle */
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
+
+class CollaborationsService {
+  constructor(cacheService) {
+    this._pool = new Pool();
+    this._cacheService = cacheService;
+  }
+
+  async addCollaboration(playlistId, userId) {
+    const id = `collab-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
+      values: [id, playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('Kolaborasi gagal ditambahkan');
+    }
+    return result.rows[0].id;
+  }
+
+  async deleteCollaboration(playlistId, userId) {
+    const query = {
+      text: 'DELETE FROM collaborations WHERE playlist_id = $1 AND user_id = $2 RETURNING id',
+      values: [playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('Kolaborasi gagal dihapus');
+    }
+  }
+
+  async verifyCollaborator(playlistId, userId) {
+    const query = {
+      text: 'SELECT * FROM collaborations WHERE playlist_id = $1 AND user_id = $2',
+      values: [playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new AuthorizationError('Kolaborasi gagal diverifikasi');
+    }
+  }
+}
+
+module.exports = CollaborationsService;
